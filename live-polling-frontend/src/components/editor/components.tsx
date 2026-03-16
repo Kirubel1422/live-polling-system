c48656d561e0111acfe3bab0932bd5c3206e28c1
Kirubel1422 <kirubel739@gmail.com>
2026-03-16 23:07:59 +0400

    feat(editor): create reusable sub-components and barrel export

---
+++ live-polling-frontend/src/components/editor/components.tsx
@@ -0,0 +1,6 @@
+export { default as SlideCanvas, renderSlideContent } from './SlideCanvas';
+export type { ThumbnailSize } from './types';
+export { default as SlideList } from './SlideList';
+export { default as RightPanel } from './RightPanel';
+export { default as AddSlideMenu } from './AddSlideMenu';
+export { default as SlideThumbnail } from './SlideThumbnail';
